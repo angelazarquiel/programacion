db5bd3f8298b619a9c3fb10d1e035ae9f24591f1
angel <angel@B301-PROFE.dai.red>
2013-11-13 18:45:54 +0100

    Switch

---
+++ UT3ControFlujo/src/ejerciciosinicial/OperacionesSwitch.java
@@ -0,0 +1,40 @@
+package ejerciciosinicial;
+
+import java.util.Scanner;
+
+public class OperacionesSwitch {
+
+	public static void main(String[] args) {
+		
+		
+		int n1,n2;
+		String respuesta;
+
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Dame una operación en la forma: [numero operador numero]");
+		System.out.println("Operadores: \n+-Sumar \n--Restar \n*-Multiplicar \n/-Dividir\n");
+		
+		n1= teclado.nextInt();
+		respuesta = teclado.next();
+		n2 = teclado.nextInt();
+		
+		switch (respuesta) {
+			case "+":
+				System.out.println("El resultado de la suma es: " +(n1+n2));
+				break;
+			case "-":
+				System.out.println("El resultado de la resta es: " +(n1-n2));
+				break;
+			case "*":
+				System.out.println("El resultado del producto es: " +(n1*n2));
+				break;
+			case "/":
+				System.out.println("El resultado de la división es: " + (n1/n2));
+				break;
+			default:
+				System.out.println("Error, operación no disponible.");
+		}
+	}
+
+}
