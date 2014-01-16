1fece42c33b7cf3f6151c4eaa26ca7a7308e31df
usuario <usuario@B301-PROFE.dai.red>
2014-01-16 16:26:49 +0100

    16/1

---
+++ UT3ControFlujo/src/metodos/Ejemplo14.java
@@ -0,0 +1,30 @@
+package metodos;
+import java.util.Scanner;
+
+
+public class Ejemplo14 {
+
+	public static double redondea(double a_redondear) {
+		return Math.floor(a_redondear +0.5);
+	}
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner teclado=new Scanner(System.in);
+		double numero;
+		
+		do {
+		
+			System.out.println("Dame un numero real: ");
+		
+			numero= teclado.nextDouble();
+		
+			System.out.println(numero +
+					" redondeado es: " + redondea(numero));
+			
+			
+		} while (numero != 0.0);
+
+	}
+
+}
