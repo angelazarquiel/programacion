0c09e45d984fe2df512c3d1ed52155009a2d8741
usuario <usuario@B301-PROFE.dai.red>
2014-01-16 16:39:08 +0100

    16/1

--- UT3ControFlujo/src/metodos/Ejemplo143.java
+++ UT3ControFlujo/src/metodos/Ejemplo143.java
@@ -4,14 +4,27 @@ import java.util.Scanner;
 
 public class Ejemplo143 {
 
-	public static double redondea(double a_redondear) {
-		return Math.floor(a_redondear +0.5);
+	public static double redondea(double a_redondear, int decimales) {
+		double resultado;
+		
+		resultado=a_redondear*Math.pow(10,decimales);
+		
+		resultado=Math.floor(resultado +0.5);
+		
+		resultado=resultado/Math.pow(10,decimales);
+		
+		return resultado;
 	}
 	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
 		double numero;
+		int decimales;
+		
+		System.out.println("-SuperRedondator-");
+		System.out.println("Dame un numero de decimales (entero): ");
+		decimales = teclado.nextInt();
 		
 		do {
 		
@@ -20,11 +33,12 @@ public class Ejemplo143 {
 			numero= teclado.nextDouble();
 		
 			System.out.println(numero +
-					" redondeado es: " + redondea(numero));
+					" redondeado es: " + redondea(numero,decimales));
 			
 			
 		} while (numero != 0.0);
 
+		System.out.println("Adiós!");
 	}
 
 }
