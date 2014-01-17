5d8ef00177c37fdcf73806ff0aa4063c76ef4474
angelazarquiel <amcanadas@edu.jccm.es>
2014-01-17 12:04:19 +0100

    16/1

---
+++ UT3ControFlujo/src/metodos/Ejemplo144.java
@@ -0,0 +1,25 @@
+package metodos;
+
+import java.util.Scanner;
+
+public class Ejemplo144 {
+
+	public static void main(String[] args) {
+		char caracter='*';
+		
+		String nombre="Angel";
+		
+		caracter=nombre.charAt(0);
+		
+		System.out.println("Resultado : " + caracter);
+		
+		Scanner scn=new Scanner(System.in);
+		
+		System.out.println(scn.next().charAt(0));
+		
+		Character.toString('m');
+		
+		
+	}
+
+}
