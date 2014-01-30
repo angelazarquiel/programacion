baf6c973968e6b9da69728155e165320e96624bf
usuario <usuario@B301-PROFE.dai.red>
2014-01-30 17:20:08 +0100

    30/1

---
+++ UT4Excepciones/src/Ejercicio2_4b.java
@@ -0,0 +1,52 @@
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*
+ Escribe un programa que pida diez números con decimales. Si insertan letras, 
+ se pedirá de nuevo dicho número. El programa debe visualizar el número más alto 
+ de todos los insertados. Si se produce alguna excepción se mostrará el mensaje 
+ correspondiente. Además, al final del programa se visualizará un mensaje que indica 
+ cuántas veces se ha producido dicho error.
+ */
+public class Ejercicio2_4b {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+				
+		int errores = 0;
+		double numeroIntroducido,maximo;
+		
+		maximo = -Double.MAX_VALUE;
+		System.out.println(maximo);
+
+		for (int correctos = 0; correctos < 10;) {
+			try {
+				if (correctos != 0) {
+					System.out.println("Inserte otro número: ");
+				} else {
+					System.out.println("Escriba 10 números decimales: ");
+				}
+				numeroIntroducido= teclado.nextDouble();
+				if (numeroIntroducido > maximo) 
+					maximo = numeroIntroducido;
+				correctos++;
+			} catch (InputMismatchException ime) {
+				System.out.println("\nError: " + ime.toString());
+				System.out.println("Tipo de error: " + ime.getMessage()
+						+ "\nSe han introducido caracteres "
+						+ "para un valor de tipo numérico.\n");
+				teclado.nextLine();
+				errores++;
+			}
+		}
+		
+		System.out.println("El valor más alto introducido es el número: " + maximo + ".");
+		System.out.println("Se han producido un total de: " + errores 
+				+ " errores, durante la ejecución del programa.");
+
+		teclado.close();
+	}
+
+}
