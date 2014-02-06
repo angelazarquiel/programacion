8693b57f3a916f64c0a0ffa1105739a9557d8fea
angelazarquiel <amcanadas@edu.jccm.es>
2014-02-06 10:25:04 +0100

    6/2

--- UT5Clases/src/ejemplos/Cuenta.java
+++ UT5Clases/src/ejemplos/Cuenta.java
@@ -1,19 +1,27 @@
 package ejemplos;
 
 public class Cuenta {
+	
 	private double saldo=0;
+	private long num_transacciones=0;
 	
 	public void ingresar(double cantidad) {
 		saldo+=cantidad;
+		num_transacciones++;
 	}
 	
 	public void sacar(double cantidad) {
 		saldo-=cantidad;
 		if (saldo<0)
-			System.out.println("La cuenta esta en rojo");
+			System.out.println("¡La cuenta esta en números rojos!");
+		num_transacciones++;
 	}
 
 	public double getSaldo() {
 		return saldo;
+	}
+	
+	public long getTransacciones() {
+		return num_transacciones;
 	}
 }
--- UT5Clases/src/ejemplos/PruebaCuenta.java
+++ UT5Clases/src/ejemplos/PruebaCuenta.java
@@ -1,14 +1,21 @@
 package ejemplos;
 
 public class PruebaCuenta {
-
-	public void main(String []args){
+	
+	public static void main(String[] args){
 		Cuenta mi_cuenta;
 		
 		mi_cuenta= new Cuenta();
 		
 		mi_cuenta.ingresar(1000);
 		
-		System.out.println(mi_cuenta.getSaldo());
+		System.out.println("Saldo inicial: " + mi_cuenta.getSaldo());
+		
+		mi_cuenta.sacar(200);
+		mi_cuenta.sacar(100);
+		mi_cuenta.ingresar(100);
+		
+		System.out.println("Saldo actual: " + mi_cuenta.getSaldo());
+		System.out.println("Número de movimientos: " + mi_cuenta.getTransacciones());
 	}
 }
